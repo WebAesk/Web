dc75df23a9532cf4ec0631b15b3025f01da64a5d
Furkan Baran <furkanbarann5@gmail.com>
2024-12-23 15:29:58 +0300

    SADASD

--- src/Pages/CarPage.jsx
+++ src/Pages/CarPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/car.css';
+import '../css/Car.css';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import Cars from "../components/Cars.jsx";
